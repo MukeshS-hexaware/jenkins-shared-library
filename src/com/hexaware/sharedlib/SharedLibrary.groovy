10c42dae2ebf0fc0d1e2521260d095db915c62fc
MukeshS-hexaware <85923791+MukeshS-hexaware@users.noreply.github.com>
2021-12-17 14:28:17 +0530

    Create SharedLibrary.groovy

---
+++ src/com/hexaware/sharedlib/SharedLibrary.groovy
@@ -0,0 +1,11 @@
+public class SharedLibrary {
+  def steps
+  
+  public SharedLibrary(steps) {
+    this.steps = steps
+  }
+  
+  public void startBuild() {
+    steps.echo "Hello, World!"
+  }
+}
